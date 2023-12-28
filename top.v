0d4a1ea025971764a0395da90e951c9addf1bd0e
Star0228 <3220104364@zju.edu.cn>
2023-12-28 18:23:50 +0800

    2023.12.28.18.23

--- top.v
+++ top.v
@@ -50,7 +50,7 @@
     end
    
     //Initialize the coordinate of the monsters with loop
-    integer monster_num = 2;
+    parameter monster_num = 2;
     reg [9:0]x_slim[0:monster_num-1];
     reg [8:0]y_slim[0:monster_num-1];
     initial begin
@@ -60,7 +60,7 @@
         end
     end
     //Initialize snowflake's coordinate with loop
-    integer snowflake_num = 15;
+    parameter snowflake_num = 15;
     reg [9:0]x_snowf[0:snowflake_num-1];
     reg [8:0]y_snowf[0:snowflake_num-1];
     initial begin
@@ -72,7 +72,7 @@
 
     //Initialize the coordinate of the ground with loop
     //The map is constructed by blocks
-    integer ground_num = 50;
+    parameter ground_num = 50;
     reg [9:0]x_ground[0:ground_num-1];
     reg [8:0]y_ground[0:ground_num-1];
     initial begin
@@ -134,15 +134,10 @@
     //蓝色小人静态图片的地址寄存器和vga输出
     reg [13:0]blue_st;
     reg [11:0]vga_blue_st;
-    //蓝色小人向右奔跑的图片的地址寄存器和vga输出
-    reg [13:0]blue_rwk;
-    reg [11:0]vga_blue_rwk;
     
     //The address of the monsters and the output of vga
     reg [11:0] vga_slim_st[0:monster_num-1];
     reg [13:0] slim_st[0:monster_num-1];
-    reg [11:0] vga_slim_fz[0:monster_num-1];
-    reg [13:0] slim_fz[0:monster_num-1];
 
     //The address of the snowflakes and the output of vga
     wire [11:0] vga_snowf[0:snowflake_num-1];
@@ -162,7 +157,6 @@
         // //怪物1静止62*36（x_slim1,y_slim1）图片自带028的背景色
         for (integer i=0;i<monster_num;i=i+1)begin
             slim_st[i]<= (col_addr_x>=x_slim[i]&&col_addr_x<=x_slim[i]+61&&row_addr_y>=y_slim[i]&&row_addr_y<=y_slim[i]+35)?(row_addr_y-y_slim[i])*62+col_addr_x-x_slim[i]:0;
-            slim_fz[i]<= (col_addr_x>=x_slim[i]&&col_addr_x<=x_slim[i]+49&&row_addr_y>=y_slim[i]&&row_addr_y<=y_slim[i]+50)?(row_addr_y-y_slim[i])*50+col_addr_x-x_slim[i]:0;
         end
 
         //雪花1图片24*26（x_snowf1,y_snowf1）图片自带028的背景色
@@ -179,57 +173,13 @@
     //调用ip核输出背景像素值
     background bg2(.clka(clk),.addra(bg),.douta(vga_bg));
 
-    //Static pixel values and frozen pixel values of the monsters
-    wire [11:0] vga_slim_st_1[0:monster_num-1],vga_slim_st_11[0:monster_num-1],vga_slim_st_13[0:monster_num-1],vga_slim_st_18[0:monster_num-1],vga_slim_st_20[0:monster_num-1],vga_slim_st_22[0:monster_num-1],vga_slim_st_24[0:monster_num-1],vga_slim_st_26[0:monster_num-1],vga_slim_st_28[0:monster_num-1],vga_slim_st_30[0:monster_num-1],vga_slim_st_32[0:monster_num-1],vga_slim_st_34[0:monster_num-1],vga_slim_st_36[0:monster_num-1],vga_slim_st_38[0:monster_num-1];
-    wire [11:0] vga_slim_fz_1[0:monster_num-1],vga_slim_fz_3[0:monster_num-1],vga_slim_fz_5[0:monster_num-1],vga_slim_fz_7[0:monster_num-1],vga_slim_fz_9[0:monster_num-1];
     //update the signal with loop
-    genvar slim_static_i;
-    generate
-        for (slim_static_i=0;slim_static_i<monster_num;slim_static_i=slim_static_i+1)begin:slim_static
-            slim_static_1 slim_st_1f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_1[slim_static_i]));
-            slim_static_11 slim_st_11f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_11[slim_static_i]));
-            slim_static_13 slim_st_13f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_13[slim_static_i]));
-            slim_static_18 slim_st_18f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_18[slim_static_i]));
-            slim_static_20 slim_st_20f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_20[slim_static_i]));
-            slim_static_22 slim_st_22f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_22[slim_static_i]));
-            slim_static_24 slim_st_24f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_24[slim_static_i]));
-            slim_static_26 slim_st_26f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_26[slim_static_i]));
-            slim_static_28 slim_st_28f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_28[slim_static_i]));
-            slim_static_30 slim_st_30f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_30[slim_static_i]));
-            slim_static_32 slim_st_32f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_32[slim_static_i]));
-            slim_static_34 slim_st_34f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_34[slim_static_i]));
-            slim_static_36 slim_st_36f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_36[slim_static_i]));
-            slim_static_38 slim_st_38f(.clka(clk),.addra(slim_st[slim_static_i]),.douta(vga_slim_st_38[slim_static_i]));
-        end
-    endgenerate
-    //update the signal with loop
-    genvar slim_frozen_i;
-    generate
-        for (slim_frozen_i=0;slim_frozen_i<monster_num;slim_frozen_i=slim_frozen_i+1)begin:slim_frozen
-            slim_frozen_1 slim_fz_1f(.clka(clk),.addra(slim_fz[slim_frozen_i]),.douta(vga_slim_fz_1[slim_frozen_i]));
-            slim_frozen_3 slim_fz_3f(.clka(clk),.addra(slim_fz[slim_frozen_i]),.douta(vga_slim_fz_3[slim_frozen_i]));
-            slim_frozen_5 slim_fz_5f(.clka(clk),.addra(slim_fz[slim_frozen_i]),.douta(vga_slim_fz_5[slim_frozen_i]));
-            slim_frozen_7 slim_fz_7f(.clka(clk),.addra(slim_fz[slim_frozen_i]),.douta(vga_slim_fz_7[slim_frozen_i]));
-            slim_frozen_9 slim_fz_9f(.clka(clk),.addra(slim_fz[slim_frozen_i]),.douta(vga_slim_fz_9[slim_frozen_i]));
-        end
-    endgenerate
-
-
-    //雪花1像素值
-
     //renew the image
     reg [31:0]ipcnt;
-    reg [4:0]ip_blue_st;
-    reg [4:0]ip_blue_rwk;
-    reg [7:0] ip_slim_st[0:monster_num-1];
     //这里便能控制单次刷新后保持不变
     always @(posedge clk) begin
         if(ipcnt == 6_000_000) begin  // 40ms*100M=4M，由于计数器只有3位，所以这里实例只能计数到500k，所以选用了40ms/5=8ms，即2.5*100k
             ipcnt <= 0;
-            ip_blue_st <= ip_blue_st + 1;
-            ip_slim1_st <= ip_slim1_st + 1;
-            ip_slim2_st <= ip_slim2_st + 1;
-            ip_blue_rwk <= ip_blue_rwk + 1;
         end
         else begin
             ipcnt <= ipcnt + 1;
@@ -259,7 +209,7 @@
             vga_data<=vga_bg[11:0];   
         end
         //2.方块
-        for(i=0;i<ground_num;i=i+1)begin
+        for(integer i=0;i<ground_num;i=i+1)begin
             if(col_addr_x>=x_ground[i]&&col_addr_x<=x_ground[i]+27&&row_addr_y>=y_ground[i]&&row_addr_y<=y_ground[i]+41)begin
                 if(vga_ground[i][11:0]!=0*256+2*16+8)begin
                     vga_data<=vga_ground[i][11:0];   
@@ -269,7 +219,7 @@
         //3.树
         //4.小石块
         //5. snowflakes
-        for(i=0;i<snowflake_num;i=i+1)begin
+        for(integer i=0;i<snowflake_num;i=i+1)begin
             if(col_addr_x>=x_snowf[i]&&col_addr_x<=x_snowf[i]+23&&row_addr_y>=y_snowf[i]&&row_addr_y<=y_snowf[i]+25)begin
                 if(vga_snowf[i][11:0]!=0*256+2*16+8)begin
                     vga_data<=vga_snowf[i][11:0];   
@@ -278,14 +228,14 @@
         end
 
         //6.怪物62*36 028
-        if(col_addr_x>=x_slim1&&col_addr_x<=x_slim1+61&&row_addr_y>=y_slim1&&row_addr_y<=y_slim1+35)begin
-            if(vga_slim1_st[11:0]!=0*256+2*16+8)begin
-                vga_data<=vga_slim1_st[11:0];   
+        if(col_addr_x>=x_slim[0]&&col_addr_x<=x_slim[0]+61&&row_addr_y>=y_slim[0]&&row_addr_y<=y_slim[0]+35)begin
+            if(vga_slim_st[0]!=0*256+2*16+8)begin
+                vga_data<=vga_slim_st[0];   
             end
         end
-        if(col_addr_x>=x_slim2&&col_addr_x<=x_slim2+61&&row_addr_y>=y_slim2&&row_addr_y<=y_slim2+35)begin
-            if(vga_slim2_st[11:0]!=0*256+2*16+8)begin
-                vga_data<=vga_slim2_st[11:0];   
+        if(col_addr_x>=x_slim[1]&&col_addr_x<=x_slim[1]+61&&row_addr_y>=y_slim[1]&&row_addr_y<=y_slim[1]+35)begin
+            if(vga_slim_st[1]!=0*256+2*16+8)begin
+                vga_data<=vga_slim_st[0];   
             end
         end
 
